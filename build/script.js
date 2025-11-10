1484f3c2cf284d9a8515fdc3e1a41984d238785f
Mathias Moen <MathiasBenoni@github.com>
2025-11-10 10:43:09 +0100

    Changed player from circle to rect

--- build/script.js
+++ build/script.js
@@ -10,10 +10,22 @@ async function sendDirection(dir) {
 }
 
 document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowUp") sendDirection("up");
-  if (e.key === "ArrowDown") sendDirection("down");
-  if (e.key === "ArrowLeft") sendDirection("left");
-  if (e.key === "ArrowRight") sendDirection("right");
+  if (e.key === "w") {
+    sendDirection("up");
+    console.log("Up");
+  }
+  if (e.key === "s") {
+    sendDirection("down");
+    console.log("Down");
+  }
+  if (e.key === "a") {
+    sendDirection("left");
+    console.log("Left");
+  }
+  if (e.key === "d") {
+    sendDirection("right");
+    console.log("Right");
+  }
 });
 
 async function drawLoop() {
@@ -21,11 +33,11 @@ async function drawLoop() {
   const pos = await res.json();
 
   ctx.clearRect(0, 0, 400, 400);
-  ctx.beginPath();
-  ctx.arc(pos.x, pos.y, 20, 0, 2 * Math.PI);
+
+  const width = 40;
+  const height = 40;
   ctx.fillStyle = "blue";
-  ctx.fill();
-  ctx.closePath();
+  ctx.fillRect(pos.x - width / 2, pos.y - height / 2, width, height);
 
   requestAnimationFrame(drawLoop);
 }
