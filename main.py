ad6959b03a8113e0ef5443bc04dd9c89ff04abf7
Mathias Moen <MathiasBenoni@github.com>
2025-11-11 12:48:35 +0100

    added fruit logic

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify, request, send_from_directory
+import random 
+
 
 app = Flask(__name__, static_folder="build")
+
+rutestorrelse = 40
+
+brett_y = 400
+brett_x = 400
+
+brett_x_ruter = int(brett_x / rutestorrelse)
+brett_y_ruter = int(brett_y / rutestorrelse)
 
 position = {"x": 200, "y": 200}
 speed = 40
@@ -15,26 +25,62 @@ def script():
     # sender JS fra build-mappen
     return send_from_directory(app.static_folder, "script.js")
 
+
+def eat_fruit():
+    pass
+
+fruits = []
+
+def spawn_fruit():
+    valid_tiles = []
+
+    player_tile_x = int(position["x"] / rutestorrelse)
+    player_tile_y = int(position["y"] / rutestorrelse)
+    
+    for x in range(brett_x_ruter):
+        for y in range(brett_y_ruter):
+            if not (x == player_tile_x and y == player_tile_y):
+                valid_tiles.append((x, y))
+
+    if not valid_tiles:
+        return None
+
+    tile = random.choice(valid_tiles)
+    fruits.append({"x": tile[0], "y": tile[1]})
+    return tile
+
+@app.route("/fruits", methods=["GET"])
+def get_fruits():
+    return jsonify(fruits)
+
+@app.route("/spawn_fruit", methods=["POST"])
+def spawn_one():
+    fruit = spawn_fruit()
+    return jsonify(fruit)
+
+
 @app.route("/move", methods=["POST"])
 def move():
+    
     global position
     data = request.json
     x, y = position["x"], position["y"]
     direction = data.get("direction")
-    if direction == "up":
+    if direction == "up" and position["y"] >= 40:
         y -= speed
-    elif direction == "down":
+    elif direction == "down" and position["y"] <= 320:
         y += speed
-    elif direction == "left":
+    elif direction == "left" and position["x"] >= 40:
         x -= speed
-    elif direction == "right":
+    elif direction == "right" and position["x"] <= 320:
         x += speed
     position["x"], position["y"] = x, y
+
+
     return jsonify(position)
 
 @app.route("/position")
 def get_position():
     return jsonify(position)
-
 if __name__ == "__main__":
     app.run(debug=True)
